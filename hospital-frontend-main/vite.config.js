3d5cf7ddefc762a0531f7099495e931eb5d099b6
2300031388 <2300031388@kluniversity.in>
2025-10-29 15:30:11 +0530

    added Jenkinsfile

--- hospital-frontend-main/vite.config.js
+++ hospital-frontend-main/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/CICD_Exam/',  // 👈 this must match your repo name
+  base: '/CICD_Exam/',
 })
